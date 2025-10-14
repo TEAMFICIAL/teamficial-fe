b370191d38c1be391b52a3a6cb2df726797752c5
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-10-14 22:35:08 +0900

    #10 [CHORE] red 색상 변경

--- src/app/(main)/recruit/_components/TitleInput.tsx
+++ src/app/(main)/recruit/_components/TitleInput.tsx
@@ -7,12 +7,12 @@ const TitleInput = () => {
   return (
     <div className="flex flex-col gap-2">
       <input
-        className={`body-2 h-18 rounded-lg border-1 border-gray-300 px-8 text-gray-800 placeholder:text-gray-500 ${title.length > 30 ? 'border-red-500' : 'focus:border-gray-600'}`}
+        className={`body-2 h-18 rounded-lg border-1 border-gray-300 px-8 text-gray-800 placeholder:text-gray-500 ${title.length > 30 ? 'border-red-100' : 'focus:border-gray-600'}`}
         placeholder="제목을 입력해주세요 (최대30자)"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <span className={`body-8 text-gray-500 ${title.length > 30 ? 'text-red-500' : ''}`}>
+      <span className={`body-8 text-gray-500 ${title.length > 30 ? 'text-red-100' : ''}`}>
         {title.length > 30 ? '제목을 30자 이하로 입력해주세요.' : ''}
       </span>
     </div>
