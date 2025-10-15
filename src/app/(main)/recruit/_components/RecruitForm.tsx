03365ecedf1932803c9bfaaee01dbc244be45a3d
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-10-16 01:14:43 +0900

    #10 [FEAT] 날짜 지정 에러 처리

--- src/app/(main)/recruit/_components/RecruitForm.tsx
+++ src/app/(main)/recruit/_components/RecruitForm.tsx
@@ -7,22 +7,31 @@ import RecruitPosition from './RecruitPosition';
 import ProjectDate from './ProjectDate';
 
 const RecruitForm = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  const isEndDateInvalid = startDate && endDate && endDate > startDate;
+
   return (
     <>
       <div className="flex flex-col gap-3">
         <TitleInput />
-        <div className="flex flex-col gap-10 rounded-2xl border-1 border-gray-300 p-8">
+        <div className="flex flex-col gap-10 rounded-2xl border-1 border-gray-300 p-10">
           {/* 모집분야/인원 */}
           <RecruitPosition />
           {/* 진행방법 */}
           <ProcessMethod />
           {/* 프로젝트 기간 및 연락처 */}
           <div className="flex flex-col gap-6">
-            <ProjectDate title="프로젝트 시작 예정일" />
+            <ProjectDate title="프로젝트 시작 예정일" date={startDate} setDate={setStartDate} />
             {/* 프로젝트 진행기간 */}
             <ProjectDuration />
-            <ProjectDate title="공고 마감일" />
-
+            <ProjectDate
+              title="공고 마감일"
+              date={endDate}
+              setDate={setEndDate}
+              error={isEndDateInvalid ? '공고 마감일은 프로젝트 시작 예정일 이후여야 합니다.' : ''}
+            />
             <div className="flex flex-col gap-4">
               <p className="title-3">연락 방법</p>
               <input
@@ -34,7 +43,7 @@ const RecruitForm = () => {
         </div>
         <div className="flex rounded-2xl border-1 border-gray-300 px-8 py-4">글 작성 구역</div>
       </div>
-      <div className="mt-6 flex justify-end">
+      <div className="mt-6 mb-10 flex justify-end">
         <button className="bg-primary-900 text-gray-0 body-3 w-48 rounded-lg py-4">
           업로드하기
         </button>
