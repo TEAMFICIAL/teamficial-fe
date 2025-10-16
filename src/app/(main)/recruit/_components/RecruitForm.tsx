8a0fecf4bbb7fec730c5e8464b41d0e52c42fc2c
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-10-17 00:00:42 +0900

    #12 [FEAT] 글 작성구역 컴포넌트 분리

--- src/app/(main)/recruit/_components/RecruitForm.tsx
+++ src/app/(main)/recruit/_components/RecruitForm.tsx
@@ -6,6 +6,7 @@ import ProjectDuration from './ProjectDuration';
 import RecruitPosition from './RecruitPosition';
 import ProjectDate from './ProjectDate';
 import TextInput from './TextInput';
+import TextContent from './TextContent';
 
 const RecruitForm = () => {
   const [startDate, setStartDate] = useState<Date | null>(null);
@@ -38,7 +39,7 @@ const RecruitForm = () => {
             <TextInput title="연락 방법" />
           </div>
         </div>
-        <div className="flex rounded-2xl border-1 border-gray-300 px-8 py-4">글 작성 구역</div>
+        <TextContent />
       </div>
       {/* 추후 컴포넌트 분리 */}
       <div className="mt-6 mb-10 flex justify-end">
---
+++ src/app/(main)/recruit/_components/TextContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextContent = () => {
+  return <div className="flex rounded-2xl border-1 border-gray-300 px-8 py-4">글 작성 구역</div>;
+};
+
+export default TextContent;
