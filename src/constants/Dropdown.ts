080cd163dd10fb5bebb78942c9418f494d74a210
김지원 <jiwonkim0531@gachon.ac.kr>
2025-10-25 00:53:56 +0900

    #14 [MOVE] 드롭다운 상수 분리

---
+++ src/constants/Dropdown.ts
@@ -0,0 +1,20 @@
+export const RECRUIT_OPTIONS = [
+  { label: '프론트엔드', value: 'frontend' },
+  { label: '백엔드', value: 'backend' },
+  { label: 'UX/UI디자인', value: 'uxui' },
+  { label: 'AI', value: 'ai' },
+  { label: '안드로이드', value: 'android' },
+  { label: 'iOS', value: 'ios' },
+  { label: '기획', value: 'planning' },
+  { label: '마케팅', value: 'marketing' },
+  { label: 'PM', value: 'pm' },
+  { label: '클라우드', value: 'cloud' },
+  { label: '인프라', value: 'infrastructure' },
+  { label: '데브옵스', value: 'devops' },
+];
+
+export const DURATION_OPTIONS = [
+  { label: '온라인', value: 'online' },
+  { label: '오프라인', value: 'offline' },
+  { label: '온/오프라인', value: 'onoff' },
+];
