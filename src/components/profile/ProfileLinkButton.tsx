89154e0bd15e4c9be2e577823aad9705e24751fe
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-10-26 21:35:18 +0900

    #16 [CHORE] 버튼 접근성 추가

--- src/components/profile/ProfileLinkButton.tsx
+++ src/components/profile/ProfileLinkButton.tsx
@@ -9,7 +9,12 @@ type LinkButtonProps = {
 
 const ProfileLinkButton = ({ type, link }: LinkButtonProps) => {
   return (
-    <button className="h-7 w-7 cursor-pointer" onClick={() => link && window.open(link, '_blank')}>
+    <button
+      aria-label="프로필 버튼"
+      disabled={!link}
+      className="h-7 w-7 cursor-pointer"
+      onClick={() => link && window.open(link, '_blank')}
+    >
       <Image src={`/icons/${type}.svg`} alt={type} width={28} height={28} />
     </button>
   );
