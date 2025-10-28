43b49ed0e1fc9b65377f6bf6704ca8a639af02f1
김지원 <jiwonkim0531@gachon.ac.kr>
2025-10-28 22:06:19 +0900

    #18 [FIX] 버튼 컴포넌트 disabled 속성 추가 및 padding값 제거

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -3,14 +3,16 @@ import { cn } from '@/utils/cn';
 interface ButtonProps {
   children: React.ReactNode;
   className: string;
+  disabled?: boolean;
   onClick?: () => void;
 }
 
-const Button = ({ children, className, onClick }: ButtonProps) => {
+const Button = ({ children, className, disabled, onClick }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={cn('body-5 cursor-pointer rounded-lg px-5 py-3', className)}
+      disabled={disabled}
+      className={cn('body-5 cursor-pointer rounded-lg', className)}
     >
       {children}
     </button>
