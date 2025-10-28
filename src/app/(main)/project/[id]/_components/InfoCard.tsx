1092e1f100ae06f189a4e37116f8f78bf8dc4b98
김지원 <jiwonkim0531@gachon.ac.kr>
2025-10-28 22:04:24 +0900

    #18 [FIX] 마감예정일 -> 진행기간으로 변경

--- src/app/(main)/project/[id]/_components/InfoCard.tsx
+++ src/app/(main)/project/[id]/_components/InfoCard.tsx
@@ -7,7 +7,7 @@ const InfoCard = () => {
       <InfoItem label="모집 분야/인원" value="기획 2명, 디자인 1명, 프론트엔드 2명" />
       <div className="flex">
         <InfoItem label="시작 예정일" value="2025년 10월 1일" className="flex-2" />
-        <InfoItem label="마감 예정일" value="미정/협의 예정" className="flex-1" />
+        <InfoItem label="진행 기간" value="미정/협의 예정" className="flex-1" />
       </div>
       <div className="flex">
         <InfoItem label="진행방식" value="온라인" className="flex-2" />
