fc467a1a6b08f03f502db09bec4618226961b04e
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-10-29 23:15:23 +0900

    #20 [FEAT] my-page 관련 api 스키마

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -24,3 +24,65 @@ export type ResponseProfile = {
   modifiedAt: string;
 };
 
+// /my-page 관련 API
+export type ApplicantProfile = {
+  applicationId: number;
+  content: string;
+  profile: ResponseProfile;
+};
+
+export type RecruitingPost = {
+  recruitingPostId: number;
+  recruitingPostTitle: string;
+  recruitingPostContent: string;
+  progressWay: string;
+  contactWay: string;
+  startDate: string;
+  period: string;
+  deadline: string;
+  recruitingDetails: string[];
+};
+
+export type ApplicantList = {
+  applicationId: number;
+  profileId: number;
+  applicantName: string;
+  profilePosition: string;
+  profileImage: string;
+};
+
+export type ApplicationStatus = {
+  recruitingPost: RecruitingPost;
+  applicantList: ApplicantList[];
+};
+
+export type CurrentApplicantItem = {
+  recruitingPostId: number;
+  writerName: string;
+  profileImage: string;
+  title: string;
+  tags: string[];
+  deadline: string;
+  totalApplicants: number;
+  createdAt: string;
+};
+
+export type MyApplicants = {
+  recruitingPostId: number;
+  writerName: string;
+  profileImage: string;
+  title: string;
+  tags: string[];
+  status: string;
+  period: string;
+  deadline: string;
+  createdAt: string;
+};
+
+export type PagedResponse<T> = {
+  content: T[];
+  currentPage: number;
+  totalPages: number;
+  totalElements: number;
+  hasNext: boolean;
+};
