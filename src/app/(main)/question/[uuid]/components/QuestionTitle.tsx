4b47dc8882b89f20ec39c26c2550f00a04515ea3
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-03 16:23:29 +0900

    #24 [FIX] 파트드롭다운 props 전달

--- src/app/(main)/question/[uuid]/components/QuestionTitle.tsx
+++ src/app/(main)/question/[uuid]/components/QuestionTitle.tsx
@@ -1,10 +1,15 @@
+'use client';
+
+import { useState } from 'react';
 import PartDropdown from './PartDropdown';
 
 const QuestionTitle = () => {
+  const [selectedPart, setSelectedPart] = useState<string | null>(null);
+
   return (
     <main className="flex flex-col gap-2">
       <div className="title-3 mt-7 flex items-center gap-3">
-        <PartDropdown />
+        <PartDropdown selected={selectedPart} onSelect={setSelectedPart} />
         파트에서 본 민수님
       </div>
       <p className="body-6 text-gray-700">각 질문별 원하는 질문을 하나씩 선택하여 응답해보세요</p>
