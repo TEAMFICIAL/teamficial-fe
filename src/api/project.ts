5f9df65bc511c842d61ebcb028bdd2fe9b917422
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-03 15:26:29 +0900

    #28 [FEAT] 모집 글 작성 api 구조

--- src/api/project.ts
+++ src/api/project.ts
@@ -1,11 +1,22 @@
 import { CommonResponse } from '@/types/common';
-import { ResponseProject } from '@/types/project';
+import { CreateProject, ResponseProject } from '@/types/project';
 import api from './api';
 
 export async function getSingleProject(postId: number): Promise<ResponseProject> {
   const { data } = await api.get<CommonResponse<ResponseProject>>(`recruiting-posts/${postId}`);
   if (!data.isSuccess) {
     throw new Error(data.message || 'Failed to fetch project');
+  }
+  return data.result;
+}
+
+export async function postProject(project: CreateProject): Promise<ResponseProject> {
+  const { data } = await api.post<CommonResponse<ResponseProject>>(`recruiting-posts`, {
+    ...project,
+  });
+
+  if (!data.isSuccess) {
+    throw new Error(data.message || 'Failed to create project');
   }
   return data.result;
 }
