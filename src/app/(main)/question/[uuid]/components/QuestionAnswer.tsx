e753ca1343f511aa346a27d3ba23bbfd039625a4
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-03 16:22:31 +0900

    #24 [FIX] QuestionList에서도 각 답변의 상태를 관리할 수 있도록 수정

--- src/app/(main)/question/[uuid]/components/QuestionAnswer.tsx
+++ src/app/(main)/question/[uuid]/components/QuestionAnswer.tsx
@@ -1,8 +1,16 @@
 'use client';
 
-const QuestionAnswer = () => {
+interface QuestionAnswerProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const QuestionAnswer = ({ value, onChange }: QuestionAnswerProps) => {
   return (
     <textarea
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      maxLength={250}
       placeholder="질문에 대한 답을 입력해주세요(최대 250자)"
       className="body-6 bg-gray-0 flex resize-none rounded-md border border-gray-300 px-8 pt-5 pb-7.5 text-gray-700 placeholder-gray-500 focus:border-gray-600 focus:outline-none"
     />
