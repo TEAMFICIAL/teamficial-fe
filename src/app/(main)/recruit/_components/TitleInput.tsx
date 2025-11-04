5c6b10545a43bf4217e7feb6da64a8a8bb107bb8
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-04 14:35:44 +0900

    #28 [FEAT] 제목 입력 컨트롤러

--- src/app/(main)/recruit/_components/TitleInput.tsx
+++ src/app/(main)/recruit/_components/TitleInput.tsx
@@ -1,23 +1,41 @@
 'use client';
-import React, { useState } from 'react';
+import { RecruitFormType } from '@/libs/schemas/projectSchema';
+import { title } from 'process';
+import React from 'react';
+import { Control, Controller } from 'react-hook-form';
 
-const TitleInput = () => {
-  const [title, setTitle] = useState(``);
+type Props = {
+  name: 'title';
+  control: Control<RecruitFormType>;
+  error?: string;
+};
 
+const TitleInput = ({ name, control, error }: Props) => {
   return (
-    <div className="flex flex-col gap-2">
-      <input
-        className={`body-1 h-18 rounded-lg border-1 border-gray-300 px-8 text-gray-800 placeholder:text-gray-500 ${title.length > 30 ? 'border-red-100' : 'focus:border-gray-600'}`}
-        placeholder="제목을 입력해주세요 (최대30자)"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      {title.length > 30 && (
-        <span className={`body-8 text-gray-500 ${title.length > 30 ? 'text-red-100' : ''}`}>
-          제목을 30자 이하로 입력해주세요.
-        </span>
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { value, onChange } }) => (
+        <div className="flex flex-col gap-2">
+          <input
+            className={`body-1 h-18 rounded-lg border-1 border-gray-300 px-8 text-gray-800 placeholder:text-gray-500 ${title.length > 30 ? 'border-red-100' : 'focus:border-gray-600'}`}
+            placeholder="제목을 입력해주세요 (최대30자)"
+            value={value}
+            onChange={onChange}
+          />
+          {value.length > 30 && (
+            <span className={`body-8 text-gray-500 ${value.length > 30 ? 'text-red-100' : ''}`}>
+              제목을 30자 이하로 입력해주세요.
+            </span>
+          )}
+          {error && (
+            <span className={`body-8 text-gray-500 ${value.length > 30 ? 'text-red-100' : ''}`}>
+              {error}
+            </span>
+          )}
+        </div>
       )}
-    </div>
+    />
   );
 };
 
