dd26347f8b619d34cbc8917c77a0fcba1786ce42
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-04 14:43:35 +0900

    #28 [FEAT] 진행방법 컨트롤러

--- src/app/(main)/recruit/_components/ProcessMethod.tsx
+++ src/app/(main)/recruit/_components/ProcessMethod.tsx
@@ -1,28 +1,42 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import RadioButton from './RadioButton';
+import { Control, Controller } from 'react-hook-form';
+import { RecruitFormType } from '@/libs/schemas/projectSchema';
 import { DURATION_OPTIONS } from '@/constants/Dropdown';
 
-const ProcessMethod = () => {
-  const [method, setMethod] = useState('');
+type Props = {
+  control: Control<RecruitFormType>;
+  error?: string;
+};
 
+const ProcessMethod = ({ control, error }: Props) => {
   return (
     <div className="flex flex-col gap-4">
       <p className="title-3">진행 방법</p>
-      <div className="flex gap-6">
-        {DURATION_OPTIONS.map((option) => (
-          <RadioButton
-            key={option.value}
-            id={option.value}
-            name="method"
-            value={option.value}
-            checked={method === option.value}
-            label={option.label}
-            onChange={setMethod}
-          />
-        ))}
-      </div>
+      <Controller
+        name="progressWay"
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-6">
+              {DURATION_OPTIONS.map((option) => (
+                <RadioButton
+                  key={option.value}
+                  id={option.value}
+                  name="progressWay"
+                  value={option.value}
+                  checked={value === option.value}
+                  label={option.label}
+                  onChange={onChange}
+                />
+              ))}
+            </div>
+            {error && <span className="body-6 text-red-100">{error}</span>}
+          </div>
+        )}
+      />
     </div>
   );
 };
