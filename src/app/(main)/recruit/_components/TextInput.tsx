779dee8912fe9509de91d52afc49c376e2b70b70
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-04 15:00:55 +0900

    #28 [FEAT] 연락수단 input 컨트롤러

--- src/app/(main)/recruit/_components/TextInput.tsx
+++ src/app/(main)/recruit/_components/TextInput.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
+import { Control, Controller } from 'react-hook-form';
+import { RecruitFormType } from '@/libs/schemas/projectSchema';
 
 type Props = {
   title: string;
+  name: 'contactWay';
+  control: Control<RecruitFormType>;
+  error?: string;
   placeholder?: string;
 };
 
-const TextInput = ({ title, placeholder = 'Ex. 카카오톡 오픈채팅 링크' }: Props) => {
+const TextInput = ({
+  title,
+  name,
+  control,
+  error,
+  placeholder = 'Ex. 카카오톡 오픈채팅 링크',
+}: Props) => {
   return (
     <div className="flex flex-col gap-4">
       <p className="title-3">{title}</p>
-      <input
-        className="body-6 rounded-md border-1 border-gray-300 p-2 px-7 py-3 focus:border-gray-500"
-        placeholder={placeholder}
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <div className="flex flex-col gap-2">
+            <input
+              value={value}
+              onChange={onChange}
+              className={`body-6 rounded-md border-1 p-2 px-7 py-3 focus:border-gray-500 ${
+                error ? 'border-red-100' : 'border-gray-300'
+              }`}
+              placeholder={placeholder}
+            />
+            {error && <span className="body-6 text-red-100">{error}</span>}
+          </div>
+        )}
       />
     </div>
   );
