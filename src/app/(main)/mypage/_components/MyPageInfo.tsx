37afcbfa52171e6265c756271c60d65f6c3eb8bf
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-06 23:16:44 +0900

    #43 [FEAT] 마이페이지에서 프로필, 대쉬보드 배열에 따른 컴포넌트 렌더링 설정

--- src/app/(main)/mypage/_components/MyPageInfo.tsx
+++ src/app/(main)/mypage/_components/MyPageInfo.tsx
@@ -1,22 +1,41 @@
-import MyPageTitle from './MyPageTitle';
-// import EmptyApplicantStatus from './applicant/EmptyApplicantStatus';
-// import EmptyAppliedTeam from './applied/EmptyAppliedTeam';
+'use client';
 
-// import InitialProfile from './profile/InitialProfile';
+import MyPageTitle from './MyPageTitle';
+import EmptyApplicantStatus from './applicant/EmptyApplicantStatus';
+import EmptyAppliedTeam from './applied/EmptyAppliedTeam';
+import InitialProfile from './profile/InitialProfile';
 import ProfileSlider from './profile/ProfileSlider';
 import AppliedTeamSection from './applied/AppliedTeamsSection';
 import ApplicantStatusSection from './applicant/ApplicantStatusSection';
+import { useGetProfileList } from '@/hooks/queries/useProfile';
+import { useDashboard } from '@/hooks/queries/useDashboard';
 
 const MyPageInfo = () => {
+  const { data: profiles } = useGetProfileList();
+  const hasProfileData = profiles && profiles.length > 0;
+
+  const { data: dashboard } = useDashboard();
+  const myApplications = dashboard?.myApplications ?? [];
+  const myRecruitingPost = dashboard?.myRecruitingPost ?? [];
+
+  const hasApplications = myApplications.length > 0;
+  const hasRecruitingPosts = myRecruitingPost.length > 0;
+
   return (
     <>
       <MyPageTitle />
-      {/* <InitialProfile /> */}
-      <ProfileSlider />
-      {/* <EmptyAppliedTeam /> */}
-      <AppliedTeamSection />
-      {/* <EmptyApplicantStatus /> */}
-      <ApplicantStatusSection />
+      {hasProfileData ? <ProfileSlider /> : <InitialProfile />}
+      {hasApplications ? (
+        <AppliedTeamSection applications={myApplications} />
+      ) : (
+        <EmptyAppliedTeam />
+      )}
+
+      {hasRecruitingPosts ? (
+        <ApplicantStatusSection recruitings={myRecruitingPost} />
+      ) : (
+        <EmptyApplicantStatus />
+      )}
     </>
   );
 };
