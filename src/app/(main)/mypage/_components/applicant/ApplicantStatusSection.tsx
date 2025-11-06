e7858849a04511b62fd22f7074d7f6a753faae79
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-06 18:06:39 +0900

    #40 [FEAT] 지원자 현황 전체 컴포넌트 분리

---
+++ src/app/(main)/mypage/_components/applicant/ApplicantStatusSection.tsx
@@ -0,0 +1,19 @@
+import ApplicantStatusCard from './ApplicantStatusCard';
+
+const ApplicantStatusSection = () => {
+  return (
+    <div className="mb-14">
+      <div className="flex w-full justify-between py-5">
+        <p className="title-2 text-gray-900">지원자 현황</p>
+        <p className="body-6 cursor-pointer self-end text-gray-700">전체보기</p>
+      </div>
+      <div className="flex gap-4">
+        <ApplicantStatusCard />
+        <ApplicantStatusCard />
+        <ApplicantStatusCard />
+      </div>
+    </div>
+  );
+};
+
+export default ApplicantStatusSection;
