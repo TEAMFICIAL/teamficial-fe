25eedd809e48682913121c259220bd0df15979a1
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-06 18:07:15 +0900

    #40 [FEAT] 내가 지원한 팀 전체 컴포넌트 분리

---
+++ src/app/(main)/mypage/_components/applied/AppliedTeamsSection.tsx
@@ -0,0 +1,19 @@
+import AppliedTeamCard from './AppliedTeamCard';
+
+const AppliedTeamSection = () => {
+  return (
+    <>
+      <div className="flex w-full justify-between py-5">
+        <p className="title-2 text-gray-900">내가 지원한 팀</p>
+        <p className="body-6 cursor-pointer self-end text-gray-700">전체보기</p>
+      </div>
+      <div className="mb-3 flex gap-4">
+        <AppliedTeamCard />
+        <AppliedTeamCard />
+        <AppliedTeamCard />
+      </div>
+    </>
+  );
+};
+
+export default AppliedTeamSection;
