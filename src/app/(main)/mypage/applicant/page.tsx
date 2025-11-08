c42bf8c901889b0c7a8d9bd35875041d2c25adaa
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-08 23:43:32 +0900

    #46 [FEAT] 지원자 현황 전체 페이지에서 해당 컴포넌트로 props 전달

--- src/app/(main)/mypage/applicant/page.tsx
+++ src/app/(main)/mypage/applicant/page.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import { useState } from 'react';
 import ApplicantTeamList from './_components/ApplicantTeamList';
 import ApplicantTitle from './_components/ApplicantTitle';
 
 const Page = () => {
+  const [status, setStatus] = useState('');
+  const [page, setPage] = useState(1);
+
   return (
     <>
-      <ApplicantTitle />
-      <ApplicantTeamList />
+      <ApplicantTitle applicantStatus={status} setApplicantStatus={setStatus} />
+      <ApplicantTeamList status={status} page={page} setPage={setPage} />
     </>
   );
 };
