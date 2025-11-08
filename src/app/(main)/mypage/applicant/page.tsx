e7f8d86c9eb039c72cd227fadfa29a646fae0334
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-08 18:01:17 +0900

    #36 [FEAT] 지원자 현황 페이지 구현

---
+++ src/app/(main)/mypage/applicant/page.tsx
@@ -0,0 +1,13 @@
+import ApplicantTeamList from './_components/ApplicantTeamList';
+import ApplicantTitle from './_components/ApplicantTitle';
+
+const Page = () => {
+  return (
+    <>
+      <ApplicantTitle />
+      <ApplicantTeamList />
+    </>
+  );
+};
+
+export default Page;
