21402055a052dd57104422584b39debe6c550bbb
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-08 23:45:09 +0900

    #46 [MOVE] dashboard 관련 라우팅 경로 변경

--- src/hooks/queries/useDashboard.ts
+++ src/hooks/queries/useDashboard.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
-import { getDashboard } from '@/libs/api/dashboard';
+import { getDashboard } from '@/libs/api/mypage/dashboard';
 import { ResponseDashboard } from '@/types/project';
 
 export const useDashboard = () => {
