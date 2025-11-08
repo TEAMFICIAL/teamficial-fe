843249783dec9d604300487bfb05579a19945073
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-08 23:47:21 +0900

    #46 [FEAT] 내가 지원한 팀 props 전달

--- src/app/(main)/mypage/applied/_components/AppliedTitle.tsx
+++ src/app/(main)/mypage/applied/_components/AppliedTitle.tsx
@@ -2,19 +2,21 @@
 
 import DropdownSmall from '@/components/common/DropdownSmall';
 import { RECRUIT_STATUS } from '@/constants/Dropdown';
-import { useState } from 'react';
 
-const AppliedTitle = () => {
-  const [recruitStatus, setRecruitStatus] = useState<string>('');
+interface AppliedTitleProps {
+  applicantStatus: string;
+  setApplicantStatus: (v: string) => void;
+}
 
+const AppliedTitle = ({ applicantStatus, setApplicantStatus }: AppliedTitleProps) => {
   return (
     <div className="flex flex-col pb-5">
       <p className="title-2 pt-7 pb-5 text-gray-900">내가 지원한 팀</p>
       <DropdownSmall
         name="recruitStatus"
-        value={recruitStatus}
+        value={applicantStatus}
         placeholder="지원현황"
-        onChange={(v) => setRecruitStatus(v)}
+        onChange={(v) => setApplicantStatus(v)}
         options={RECRUIT_STATUS}
       />
     </div>
