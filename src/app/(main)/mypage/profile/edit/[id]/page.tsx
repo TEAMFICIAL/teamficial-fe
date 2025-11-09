2de4e1099dc76e82490565833aed081bd6334d43
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-09 16:53:08 +0900

    #48 [FEAT] 프로필 수정하기 페이지 전체 UI 구현

---
+++ src/app/(main)/mypage/profile/edit/[id]/page.tsx
@@ -0,0 +1,15 @@
+import ProfileTitle from '../../_components/ProfileTitle';
+import ProfileEditContainer from './_components/ProfileEditContainer';
+import ProfileImage from './_components/ProfileImage';
+
+const Page = () => {
+  return (
+    <>
+      <ProfileTitle />
+      <ProfileImage />
+      <ProfileEditContainer />
+    </>
+  );
+};
+
+export default Page;
