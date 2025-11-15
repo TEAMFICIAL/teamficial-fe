2baf6edb090704320d507b21b858834b3ebe351f
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-15 12:28:26 +0900

    #51 [CHORE] 수정하기 페이지 라이팅 변경

--- src/app/(main)/project/[id]/edit/page.tsx
+++ src/app/(main)/project/[id]/edit/page.tsx
@@ -13,10 +13,9 @@ const page = async ({ params }: Props) => {
 
   return (
     <>
-      {/* TODO: 라이팅 확인 */}
       <div className="flex flex-col pt-7 pb-5">
-        <p className="title-2 text-gray-900">팀원 모집글 수정</p>
-        <p className="body-6 text-gray-700">게시글을 수정하여 원하는 분야의 팀원을 모집해보세요</p>
+        <p className="title-2 text-gray-900">팀원 모집하기</p>
+        <p className="body-6 text-gray-700">게시글을 작성하여 원하는 분야의 팀원을 모집해보세요</p>
       </div>
       <FormEditor postId={postId} />
     </>
