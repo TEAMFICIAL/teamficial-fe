032214e0b1c2fd00c4067f8900b793d71203492a
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-15 12:51:10 +0900

    #53 [FEAT] 추가된 배너 이미지 설정 및 route 설정

--- src/app/(main)/project/_components/Banner.tsx
+++ src/app/(main)/project/_components/Banner.tsx
@@ -1,15 +1,22 @@
 import LineButton from '@/components/common/button/LineButton';
+import { useRouter } from 'next/navigation';
 
 const Banner = () => {
+  const router = useRouter();
+
   return (
-    <main className="flex h-[235px] flex-col gap-10 rounded-2xl bg-gray-200 p-10">
-      <p className="title-2">
+    <div
+      className="relative flex h-[235px] w-[944px] flex-col gap-10 rounded-2xl bg-cover bg-center p-10"
+      style={{ backgroundImage: `url('/icons/banner.svg')` }}
+    >
+      <p className="title-2 text-gray-900">
         팀피셜록에서 얻은 키워드로
         <br />
         프로젝트를 함께 할 동료를 찾아보세요
       </p>
-      <LineButton>팀피셜록 요청하기 →</LineButton>
-    </main>
+
+      <LineButton onClick={() => router.push(`/teampsylog`)}>팀피셜록 요청하기 →</LineButton>
+    </div>
   );
 };
 
