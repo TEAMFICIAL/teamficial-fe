1ffb58f8af989c1d03bfe26cb69f73bfbcf8832a
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-15 12:56:51 +0900

    #53 [FEAT] 프로필 전체 페이지에서 api 불러오고 컴포넌트 props로 전달

--- src/app/(main)/mypage/profile/edit/[id]/page.tsx
+++ src/app/(main)/mypage/profile/edit/[id]/page.tsx
@@ -1,13 +1,25 @@
+'use client';
+
+import { useParams } from 'next/navigation';
 import ProfileTitle from '../../_components/ProfileTitle';
 import ProfileEditContainer from './_components/ProfileEditContainer';
 import ProfileImage from './_components/ProfileImage';
+import { useGetProfile } from '@/hooks/queries/useProfile';
 
 const Page = () => {
+  const { id } = useParams();
+  const profileId = Number(id);
+  const { data: profile } = useGetProfile({ profileId });
+
+  if (!profile) {
+    return <p className="text-gray-500">프로필 정보를 불러오는 중...</p>;
+  }
+
   return (
     <>
       <ProfileTitle />
-      <ProfileImage />
-      <ProfileEditContainer />
+      <ProfileImage profile={profile} />
+      <ProfileEditContainer profile={profile} />
     </>
   );
 };
