030400c8426ca08e420f7fad8368ccfa5ed9aa86
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-17 20:08:10 +0900

    #57 [FEAT] 모바일 헤더

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Header />
-      <main className="mx-auto min-h-[calc(100vh-65px)] w-full max-w-[1024px] px-10">
+      <main className="desktop:max-w-[1024px] tablet:max-w-[640px] mx-auto min-h-[calc(100vh-65px)] w-full px-10">
         {children}
       </main>
       <Footer />
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -13,42 +13,59 @@ const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="mx-auto flex w-full max-w-[1024px] justify-between px-10 pt-5">
-      <div className="flex items-center">
+    <>
+      {/* PC */}
+      <header className="tablet:flex mx-auto hidden w-full max-w-[1024px] justify-between px-10 pt-5">
+        <div className="flex items-center">
+          <Link href="/" className="flex items-center gap-2 pr-4">
+            <Image
+              src="/icons/teamficial-header.svg"
+              alt="teamficial-logo"
+              width={143}
+              height={41}
+            />
+          </Link>
+          <Link
+            href="/project"
+            className={clsx(
+              'body-4 hover:text-primary-900 px-3 transition-colors',
+              pathname.startsWith('/project') ? 'text-primary-900' : 'text-gray-900',
+            )}
+          >
+            프로젝트
+          </Link>
+          <Link
+            href="/teampsylog"
+            className={clsx(
+              'body-4 hover:text-primary-900 px-3 transition-colors',
+              pathname.startsWith('/teampsylog') ? 'text-primary-900' : 'text-gray-900',
+            )}
+          >
+            팀피셜록
+          </Link>
+        </div>
+        {isLoggedIn ? (
+          <Link href="/mypage" className="flex items-center gap-3">
+            <span className="body-6 font-semibold">{userName}님</span>
+            <Image src="/icons/profile.svg" alt="profile" width={44} height={44} />
+          </Link>
+        ) : (
+          <Link href="/login" className="flex items-center gap-3">
+            <span className="body-6">로그인/회원가입</span>
+            <Image src="/icons/profile.svg" alt="profile" width={44} height={44} />
+          </Link>
+        )}
+      </header>
+      {/* mobile */}
+      <header className="tablet:hidden mx-auto flex w-full max-w-[1024px] justify-between px-4 py-5">
         <Link href="/" className="flex items-center gap-2 pr-4">
-          <Image src="/icons/teamficial-header.svg" alt="teamficial-logo" width={143} height={41} />
+          <Image src="/icons/teamficial-header.svg" alt="teamficial-logo" width={100} height={29} />
         </Link>
-        <Link
-          href="/project"
-          className={clsx(
-            'body-4 hover:text-primary-900 px-3 transition-colors',
-            pathname.startsWith('/project') ? 'text-primary-900' : 'text-gray-900',
-          )}
-        >
-          프로젝트
-        </Link>
-        <Link
-          href="/teampsylog"
-          className={clsx(
-            'body-4 hover:text-primary-900 px-3 transition-colors',
-            pathname.startsWith('/teampsylog') ? 'text-primary-900' : 'text-gray-900',
-          )}
-        >
-          팀피셜록
-        </Link>
-      </div>
-      {isLoggedIn ? (
-        <Link href="/mypage" className="flex items-center gap-3">
-          <span className="body-6 font-semibold">{userName}님</span>
-          <Image src="/icons/profile.svg" alt="profile" width={44} height={44} />
-        </Link>
-      ) : (
-        <Link href="/login" className="flex items-center gap-3">
-          <span className="body-6">로그인/회원가입</span>
-          <Image src="/icons/profile.svg" alt="profile" width={44} height={44} />
-        </Link>
-      )}
-    </header>
+        <button>
+          <Image src="/icons/menu.svg" alt="menu" width={28} height={28} />
+        </button>
+      </header>
+    </>
   );
 };
 
