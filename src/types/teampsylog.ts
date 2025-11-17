c22552cc20a6c3c95e26ea4d55735a38be36b6db
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-18 01:50:27 +0900

    #61 [FEAT] 팀피셜록 작성하기 및 요청자 정보 타입 설정

--- src/types/teampsylog.ts
+++ src/types/teampsylog.ts
@@ -22,3 +22,21 @@ export type ResponseKeywordList = {
   totalElements: number;
   hasNext: boolean;
 };
+
+export interface RequestTeamficialLog {
+  userUuid: string;
+  content1: string;
+  content2: string;
+  content3: string;
+}
+
+export interface ResponseTeamficialLog {
+  contentPairs: {
+    keyword: string;
+    content: string;
+  }[];
+}
+
+export interface RequesterInfo {
+  requesterName: string;
+}
