c2c2e2bd025f9acd71a442159603f80e84faf3ff
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-20 22:07:18 +0900

    [FIX] #70 팀피셜록 요청하기 버튼 라우팅 설정

--- src/app/(main)/project/_components/Banner.tsx
+++ src/app/(main)/project/_components/Banner.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import LineButton from '@/components/common/button/LineButton';
+import { useUserStore } from '@/store/useUserStore';
 import { useRouter } from 'next/navigation';
 
 const Banner = () => {
   const router = useRouter();
+  const { uuid } = useUserStore();
 
   return (
     <section
-      className="relative flex h-[235px] w-[944px] flex-col gap-10 rounded-2xl bg-cover bg-center p-10"
+      className="relative flex h-[235px] max-w-[944px] flex-col gap-10 rounded-2xl bg-cover bg-center p-10"
       style={{ backgroundImage: `url('/icons/banner.svg')` }}
     >
       <p className="title-2 text-gray-900">
@@ -17,7 +19,7 @@ const Banner = () => {
         프로젝트를 함께 할 동료를 찾아보세요
       </p>
 
-      <LineButton onClick={() => router.push(`/teampsylog`)}>팀피셜록 요청하기 →</LineButton>
+      <LineButton onClick={() => router.push(`/question/${uuid}`)}>팀피셜록 요청하기 →</LineButton>
     </section>
   );
 };
