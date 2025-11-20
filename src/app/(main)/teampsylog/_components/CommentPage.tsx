9e653bc002370fd1e757102476e1f0e71a942d26
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-20 20:39:42 +0900

    #57 [FEAT] 키워드 코멘트 조회

---
+++ src/app/(main)/teampsylog/_components/CommentPage.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { useInfiniteKeywordComment } from '@/hooks/queries/useKeyword';
+import { useInView } from 'react-intersection-observer';
+import { formatDateDot } from '@/utils/project/formatDate';
+
+const CommentPage = ({ keywordId, keywordName }: { keywordId: number; keywordName: string }) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteKeywordComment({
+    keywordId,
+    size: 4,
+  });
+
+  const comments = data?.pages.flatMap((page) => page.data) || [];
+
+  const { ref, inView } = useInView();
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  return (
+    <div className="flex flex-col gap-7 p-8" style={{ overflowY: 'auto' }}>
+      <div className="flex flex-col">
+        <p className="title-4">{`‘${keywordName}’에 대한 다른 사람들의 코멘트에요`}</p>
+        <p className="body-6 text-gray-600">중복된 키워드는 말풍선이 커져요</p>
+      </div>
+      <div className="flex flex-col gap-3">
+        {comments.map((comment, idx) => (
+          <div key={idx} className="flex flex-col gap-2 rounded-[12px] bg-white px-5 pt-4 pb-5">
+            <div>
+              <p className="body-8 text-gray-500">{formatDateDot(comment.createdAt)}</p>
+              <div className="h-[1px] w-full bg-gray-300" />
+            </div>
+            <div className="body-6 text-gray-800">{comment.comment}</div>
+          </div>
+        ))}
+      </div>
+      {hasNextPage && (
+        <div ref={ref} className="py-2 text-center text-gray-400">
+          {isFetchingNextPage ? '불러오는 중...' : '아래로 스크롤하여 더 보기'}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CommentPage;
--- src/app/(main)/teampsylog/_components/LogNote.tsx
+++ src/app/(main)/teampsylog/_components/LogNote.tsx
@@ -1,6 +1,7 @@
 import { useGetKeywordList } from '@/hooks/queries/useKeyword';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
+import CommentPage from './CommentPage';
 
 const LogNote = () => {
   const [currentUserId, setCurrentUserId] = useState<number | null>(null);
@@ -26,7 +27,13 @@ const LogNote = () => {
 
   // 키워드 목록
   const keywords = data?.content?.map((item) => item.keywordName) || [];
+  const keywordIds = data?.content?.map((item) => item.keywordId) || [];
   const totalPages = Math.ceil((data?.totalElements || 1) / 6);
+
+  const [selectedKeywordId, setSelectedKeywordId] = useState<number | null>(null);
+  useEffect(() => {
+    setSelectedKeywordId(null);
+  }, [page]);
 
   return (
     <section className="relative flex">
@@ -55,7 +62,11 @@ const LogNote = () => {
           <>
             <div className="flex w-full flex-col items-center gap-2">
               {keywords.map((keyword: string, idx: number) => (
-                <div key={idx} className="body-3 w-full py-1 text-center text-gray-700">
+                <div
+                  key={idx}
+                  className="body-3 w-full cursor-pointer py-1 text-center text-gray-700"
+                  onClick={() => setSelectedKeywordId(keywordIds[idx])}
+                >
                   {keyword}
                 </div>
               ))}
@@ -64,7 +75,11 @@ const LogNote = () => {
         )}
       </div>
       {/* 오른쪽 페이지 */}
-      <div className="relative flex h-162 w-118 flex-col items-center justify-center gap-[13px] rounded-r-[16px] bg-gray-200 shadow-[0_4px_4px_0_#E1E1E1]">
+      <div
+        className={`relative flex h-162 w-118 flex-col gap-[13px] rounded-r-[16px] bg-gray-200 shadow-[0_4px_4px_0_#E1E1E1] ${
+          !selectedKeywordId ? 'items-center justify-center' : ''
+        }`}
+      >
         <button
           onClick={() => setPage((p) => (p + 1 < totalPages ? p + 1 : p))}
           disabled={page + 1 >= totalPages}
@@ -72,15 +87,24 @@ const LogNote = () => {
         >
           <Image src="/icons/page-after.svg" alt="다음 페이지" width={32} height={32} />
         </button>
-        <Image
-          src="/icons/gray_teamficial_symbol.svg"
-          alt="teamficial_symbol"
-          width={66}
-          height={67}
-        />
-        <p className="body-3 text-center whitespace-pre-line text-gray-500">
-          키워드를 선택해 자세한 내용을 확인하세요
-        </p>
+        {!selectedKeywordId ? (
+          <>
+            <Image
+              src="/icons/gray_teamficial_symbol.svg"
+              alt="teamficial_symbol"
+              width={66}
+              height={67}
+            />
+            <p className="body-3 text-center whitespace-pre-line text-gray-500">
+              키워드를 선택해 자세한 내용을 확인하세요
+            </p>
+          </>
+        ) : (
+          <CommentPage
+            keywordName={keywords[keywordIds.findIndex((id) => id === selectedKeywordId)] ?? ''}
+            keywordId={selectedKeywordId}
+          />
+        )}
       </div>
     </section>
   );
