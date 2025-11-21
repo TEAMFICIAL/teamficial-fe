6a0bb52e352e950bac1034e5e66d00c39f9a4ea6
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-21 12:27:18 +0900

    #77 [FEAT] 기본 toast 디자인 구현

---
+++ src/components/common/ToastContainer.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ToastMessage } from '@/contexts/ToastContext';
+
+export default function ToastContainer({ toast }: { toast: ToastMessage }) {
+  const base =
+    toast.type === 'success'
+      ? 'bg-green-500 text-white'
+      : toast.type === 'error'
+        ? 'bg-red-500 text-white'
+        : 'bg-gray-0 text-gray-700';
+
+  return (
+    <div className={`${base} animate-fadeInUp w-[383px] rounded-[58px] px-8 py-4 text-center`}>
+      {toast.message}
+    </div>
+  );
+}
