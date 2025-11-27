2832735a3e45cc308968afc66053cbd461e1d163
김지원 <jiwonkim0531@gachon.ac.kr>
2025-11-27 12:45:19 +0900

    #94 [FEAT] 나의 팀 쿼리 훅 구현

---
+++ src/hooks/queries/useMyTeams.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import { getMyTeams } from '@/libs/api/mypage/applicant';
+import { useQuery } from '@tanstack/react-query';
+
+export function useMyTeams(page: number = 0, size: number = 3) {
+  return useQuery({
+    queryKey: ['myTeams', page, size],
+    queryFn: () => getMyTeams(page, size),
+  });
+}
