57a04e80b5d86512781062467f92aae6b40fffc9
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-12-16 02:42:40 +0900

    #105 [FEAT] 프로젝트 정보 반응형

--- src/app/(main)/project/[id]/_components/DDayTag.tsx
+++ src/app/(main)/project/[id]/_components/DDayTag.tsx
@@ -18,7 +18,11 @@ const DDay = ({ status, dday }: DDayProps) => {
     return '마감';
   };
 
-  return <p className="body-6 rounded-sm bg-[#FFD7D9] px-3 py-1 text-[#DA1E28]">{getText()}</p>;
+  return (
+    <p className="desktop:body-6 body-9 desktop:px-3 rounded-sm bg-[#FFD7D9] px-2 py-1 text-red-100">
+      {getText()}
+    </p>
+  );
 };
 
 export default DDay;
