2f8c34366fdb99779b6a32cb00739294c7cb2999
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-12-16 21:56:22 +0900

    #105 [FEAT] 삭제/수정 반응형

--- src/app/(main)/project/[id]/_components/ProjectTitle.tsx
+++ src/app/(main)/project/[id]/_components/ProjectTitle.tsx
@@ -3,7 +3,7 @@
 import Button from '@/components/common/Button';
 import { useModal } from '@/contexts/ModalContext';
 import { ResponseProject } from '@/types/project';
-import React from 'react';
+import React, { useState } from 'react';
 import DDay from './DDayTag';
 import Link from 'next/link';
 import { useUserStore } from '@/store/useUserStore';
@@ -25,6 +25,7 @@ const ProjectTitle = ({
   const { userName } = useUserStore();
   const isLoggedIn = !!userName;
   const navigate = useRouter();
+
   const handleApplyModal = () => {
     if (!isLoggedIn) {
       window.alert('로그인이 필요합니다.');
@@ -33,8 +34,15 @@ const ProjectTitle = ({
       openModal('apply', { postId, recruitingPositions });
     }
   };
+
   const handleDeleteModal = () => {
     openModal('delete', { postId: postId, projectName: title });
+  };
+
+  const [isMoreOpen, setIsMoreOpen] = useState(false);
+
+  const handleMobileMore = () => {
+    setIsMoreOpen(!isMoreOpen);
   };
 
   return (
@@ -62,18 +70,59 @@ const ProjectTitle = ({
                 삭제
               </button>
             </div>
-            <button className="desktop:hidden block">
+            <button className="desktop:hidden block" onClick={handleMobileMore}>
               <Image src="/icons/more.svg" alt="more vertical icon" width={24} height={24} />
             </button>
+
+            {isMoreOpen && (
+              <>
+                <div className="fixed inset-0 z-50 bg-black/50" onClick={handleMobileMore} />
+                {/* 하단 버튼 영역 */}
+                <div className="fixed bottom-0 left-0 z-50 flex w-full flex-col rounded-t-2xl bg-white py-4">
+                  <Link href={`/project/${postId}/edit`}>
+                    <button
+                      className="body-7 w-full bg-gray-50 pb-5 text-gray-800"
+                      onClick={handleMobileMore}
+                    >
+                      수정하기
+                    </button>
+                  </Link>
+                  <div className="h-[1px] bg-gray-300" />
+                  <button
+                    className="body-7 w-full bg-gray-50 pt-5 text-red-100"
+                    onClick={() => {
+                      handleDeleteModal();
+                      handleMobileMore();
+                    }}
+                  >
+                    삭제하기
+                  </button>
+                </div>
+              </>
+            )}
           </>
         ) : (
-          <div className="desktop:flex hidden">
-            {alreadyApplied ? (
-              <Button label="지원완료" variant="gray" disabled size="large" />
-            ) : (
-              <Button label="지원하기" size="large" onClick={handleApplyModal} />
-            )}
-          </div>
+          <>
+            <div className="desktop:flex hidden">
+              {alreadyApplied ? (
+                <Button label="지원완료" variant="gray" disabled size="large" />
+              ) : (
+                <Button label="지원하기" size="large" onClick={handleApplyModal} />
+              )}
+            </div>
+            <div className="desktop:hidden fixed bottom-0 left-0 z-50 block w-full border-t border-gray-300 bg-white px-4 pt-3 pb-5">
+              {alreadyApplied ? (
+                <Button label="지원완료" variant="gray" disabled size="large" className="w-full" />
+              ) : (
+                <Button
+                  label="지원하기"
+                  size="large"
+                  onClick={handleApplyModal}
+                  className="w-full"
+                />
+              )}
+            </div>
+          </>
         )
       ) : null}
     </div>
