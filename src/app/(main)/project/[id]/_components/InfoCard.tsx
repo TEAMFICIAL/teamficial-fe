dccc2884831cc86a18bb0b90227b79ec3d0888df
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-12-24 23:41:24 +0900

    #105 [CHORE] 프로젝트 정보 gap 수정

--- src/app/(main)/project/[id]/_components/InfoCard.tsx
+++ src/app/(main)/project/[id]/_components/InfoCard.tsx
@@ -18,11 +18,11 @@ const InfoCard = ({
   return (
     <div className="desktop:gap-4 desktop:p-10 desktop:bg-gray-100 bg-gray-0 desktop:border-none flex flex-col gap-3 rounded-2xl border border-gray-300 p-5">
       <InfoItem label="모집 분야/인원" value={formatPosition} />
-      <div className="desktop:flex">
+      <div className="desktop:flex-row desktop:gap-0 flex flex-col gap-3">
         <InfoItem label="시작 예정일" value={formatStartDate} className="flex-2" />
         <InfoItem label="진행 기간" value={PERIOD_KR[period]} className="flex-1" />
       </div>
-      <div className="desktop:flex">
+      <div className="desktop:flex-row desktop:gap-0 flex flex-col gap-3">
         <InfoItem label="진행방식" value={PROGRESS_WAY_KR[progressWay]} className="flex-2" />
         <InfoItem label="연락방법" value={contactWay} className="flex-1" />
       </div>
