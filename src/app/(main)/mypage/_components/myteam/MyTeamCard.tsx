cd7ee3895c3e1f3ff571d667597548c532b1ebb2
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 16:47:41 +0900

    #114 [FEAT] 참여중인 팀 슬라이더 구현 및 반응형 추가

--- src/app/(main)/mypage/_components/myteam/MyTeamCard.tsx
+++ src/app/(main)/mypage/_components/myteam/MyTeamCard.tsx
@@ -18,7 +18,7 @@ const MyTeamCard = ({ myteam }: MyTeamCardProps) => {
   return (
     <div
       onClick={handleClick}
-      className="bg-gray-0 w-76 cursor-pointer rounded-2xl border border-gray-300 p-5"
+      className="bg-gray-0 tablet:w-76 w-full cursor-pointer rounded-2xl border border-gray-300 p-5"
     >
       <div className="mb-2.5 flex justify-between">
         <div className="flex gap-1">
@@ -27,8 +27,8 @@ const MyTeamCard = ({ myteam }: MyTeamCardProps) => {
         </div>
         <Tag className="body-9 bg-blue-50 text-blue-200">{myteam.totalMembers}명 참여</Tag>
       </div>
-      <p className="title-3 truncate text-gray-900">{myteam.title}</p>
-      <div className="body-8 flex gap-1 truncate text-gray-600">
+      <p className="tablet:title-3 body-5 truncate text-gray-900">{myteam.title}</p>
+      <div className="tablet:body-8 body-9 flex gap-1 truncate text-gray-600">
         {myteam.tags.map((tag) => (
           <p key={tag}>#{tag}</p>
         ))}
