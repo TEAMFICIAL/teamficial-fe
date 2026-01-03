3d7ab566c763ed8483d40640832710808a198887
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 21:30:02 +0900

    #118 [FEAT] 지원자 현황 카드 컴포넌트 반응형 구현

--- src/app/(main)/mypage/applicant/_components/ApplicantTeamCard.tsx
+++ src/app/(main)/mypage/applicant/_components/ApplicantTeamCard.tsx
@@ -22,25 +22,51 @@ const ApplicantTeamCard = ({ application, index }: ApplicantTeamCardProps) => {
   };
 
   return (
-    <button onClick={handleClick} className="flex cursor-pointer border-b border-gray-300 py-6">
-      <p className="body-6 flex w-25 items-center justify-center text-center text-gray-700">
-        {index}
-      </p>
-      <div className="flex w-153 flex-col items-start gap-2">
-        <div className="flex gap-1">
-          <Tag className="bg-primary-50 border-primary-900 text-primary-900 border">
+    <>
+      <button
+        onClick={handleClick}
+        className="tablet:hidden flex flex-col gap-2 rounded-2xl border border-gray-300 px-5 py-4"
+      >
+        <div className="flex w-full justify-between">
+          <Tag className="body-9 bg-primary-50 border-primary-900 text-primary-900 border">
             현재 {application.totalApplicants}명이 지원했어요
           </Tag>
+          <Tag className="bg-red-10 text-red-100">
+            {application.recruitingPostStatus === '모집 마감'
+              ? '마감'
+              : formatDday(application.dday)}
+          </Tag>
         </div>
-        <p className="title-3 flex w-144.75 items-start text-gray-900">{application.title}</p>
-        <p className="body-8 text-gray-600">#{application.tags.join(' #')}</p>
-      </div>
-      <div className="flex w-58 items-center justify-center">
-        <Tag className="bg-red-10 text-red-100">
-          {application.recruitingPostStatus === '모집 마감' ? '마감' : formatDday(application.dday)}
-        </Tag>
-      </div>
-    </button>
+        <div>
+          <p className="body-5 flex items-start truncate text-gray-900">{application.title}</p>
+          <p className="body-9 flex items-start text-gray-600">#{application.tags.join(' #')}</p>
+        </div>
+      </button>
+      <button
+        onClick={handleClick}
+        className="tablet:flex hidden cursor-pointer border-b border-gray-300 py-6"
+      >
+        <p className="body-6 flex w-25 items-center justify-center text-center text-gray-700">
+          {index}
+        </p>
+        <div className="flex w-153 flex-col items-start gap-2">
+          <div className="flex gap-1">
+            <Tag className="bg-primary-50 border-primary-900 text-primary-900 border">
+              현재 {application.totalApplicants}명이 지원했어요
+            </Tag>
+          </div>
+          <p className="title-3 flex w-144.75 items-start text-gray-900">{application.title}</p>
+          <p className="body-8 text-gray-600">#{application.tags.join(' #')}</p>
+        </div>
+        <div className="flex w-58 items-center justify-center">
+          <Tag className="bg-red-10 text-red-100">
+            {application.recruitingPostStatus === '모집 마감'
+              ? '마감'
+              : formatDday(application.dday)}
+          </Tag>
+        </div>
+      </button>
+    </>
   );
 };
 
