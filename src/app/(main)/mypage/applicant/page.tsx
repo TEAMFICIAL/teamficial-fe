010e5bf57fd40e0ed66beff6023792ffc0d446ef
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 21:29:42 +0900

    #118 [FEAT] 지원자현황 전체페이지 반응형 구현

--- src/app/(main)/mypage/applicant/page.tsx
+++ src/app/(main)/mypage/applicant/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import ApplicantTeamList from './_components/ApplicantTeamList';
 import ApplicantTitle from './_components/ApplicantTitle';
+import MobileHeader from '@/components/common/MobileHeader';
 
 const Page = () => {
   const [status, setStatus] = useState('');
@@ -10,8 +11,12 @@ const Page = () => {
 
   return (
     <>
-      <ApplicantTitle applicantStatus={status} setApplicantStatus={setStatus} />
-      <ApplicantTeamList status={status} page={page} setPage={setPage} />
+      <MobileHeader title="지원자 현황" />
+
+      <div className="tablet:bg-transparent -mx-4 h-full bg-gray-100">
+        <ApplicantTitle applicantStatus={status} setApplicantStatus={setStatus} />
+        <ApplicantTeamList status={status} page={page} setPage={setPage} />
+      </div>
     </>
   );
 };
