fcdb565ff2a138f9d0b732bee2e4534afbb38267
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 21:30:40 +0900

    #118 [FEAT] 내가 지원한 팀 반응형 구현

--- src/app/(main)/mypage/applied/page.tsx
+++ src/app/(main)/mypage/applied/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import AppliedTeamList from './_components/AppliedTeamList';
 import AppliedTitle from './_components/AppliedTitle';
+import MobileHeader from '@/components/common/MobileHeader';
 
 const Page = () => {
   const [status, setStatus] = useState('');
@@ -10,8 +11,11 @@ const Page = () => {
 
   return (
     <>
-      <AppliedTitle applicantStatus={status} setApplicantStatus={setStatus} />
-      <AppliedTeamList status={status} page={page} setPage={setPage} />
+      <MobileHeader title="내가 지원한 팀" />
+      <div className="tablet:bg-transparent -mx-4 h-full bg-gray-100">
+        <AppliedTitle applicantStatus={status} setApplicantStatus={setStatus} />
+        <AppliedTeamList status={status} page={page} setPage={setPage} />
+      </div>
     </>
   );
 };
