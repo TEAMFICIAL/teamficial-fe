18291c1ae6680fcb01a633cf80c05fd2df6d8576
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 21:31:47 +0900

    #118 [FEAT] 참여중인 팀 페이지 반응형 구현

--- src/app/(main)/mypage/team/page.tsx
+++ src/app/(main)/mypage/team/page.tsx
@@ -3,14 +3,18 @@
 import { useState } from 'react';
 import MyTeamTitle from './_components/MyTeamTitle';
 import MyTeamList from './_components/MyTeamList';
+import MobileHeader from '@/components/common/MobileHeader';
 
 const Page = () => {
   const [page, setPage] = useState(1);
 
   return (
     <>
-      <MyTeamTitle />
-      <MyTeamList page={page} setPage={setPage} />
+      <MobileHeader title="참여중인 팀" />
+      <div className="tablet:bg-transparent -mx-4 h-full bg-gray-100">
+        <MyTeamTitle />
+        <MyTeamList page={page} setPage={setPage} />
+      </div>
     </>
   );
 };
