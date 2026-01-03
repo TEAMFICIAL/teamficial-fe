a77d4f5f450f9f06837c184d31413c9519086bf8
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 21:31:59 +0900

    #118 [FEAT] 참여중인 팀 타이틀 반응형 구현

--- src/app/(main)/mypage/team/[id]/page.tsx
+++ src/app/(main)/mypage/team/[id]/page.tsx
@@ -5,6 +5,7 @@ import TeamMemberList from './_components/TeamMemberList';
 import TeamTitle from './_components/TeamTitle';
 import { useParams } from 'next/navigation';
 import ErrorDisplay from '@/components/common/Error';
+import MobileHeader from '@/components/common/MobileHeader';
 
 const Page = () => {
   const [status, setStatus] = useState('');
@@ -16,8 +17,11 @@ const Page = () => {
 
   return (
     <>
-      <TeamTitle teamMemberStatus={status} setTeamMemberStatus={setStatus} />
-      <TeamMemberList postId={postId} position={status} />
+      <MobileHeader title="참여중인 팀" />
+      <div className="tablet:bg-transparent -mx-4 h-full bg-gray-100">
+        <TeamTitle teamMemberStatus={status} setTeamMemberStatus={setStatus} />
+        <TeamMemberList postId={postId} position={status} />
+      </div>
     </>
   );
 };
