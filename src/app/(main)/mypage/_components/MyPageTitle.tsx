3b3ef829fb4d07da99e4fd9dc831fc893a6a860f
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 16:46:48 +0900

    #114 [FEAT] 마이페이지 /mypage 반응형 구현

--- src/app/(main)/mypage/_components/MyPageTitle.tsx
+++ src/app/(main)/mypage/_components/MyPageTitle.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const MyPageTitle = () => {
   return (
-    <div className="flex w-full justify-between pt-7 pb-5">
+    <div className="tablet:pt-7 tablet:pb-5 flex w-full justify-between pt-3 pb-4">
       <div className="flex flex-col">
-        <p className="title-2 text-gray-900">마이페이지</p>
-        <p className="body-4 text-gray-700">
+        <p className="tablet:title-2 title-4 text-gray-900">마이페이지</p>
+        <p className="tablet:body-4 body-10 text-gray-700">
           대표 키워드 수정은{' '}
           <Link href="/teampsylog" className="underline">
             팀피셜록
@@ -13,7 +13,7 @@ const MyPageTitle = () => {
           페이지에서 가능합니다
         </p>
       </div>
-      <Link href="/mypage/profile" className="body-6 self-end text-gray-700">
+      <Link href="/mypage/profile" className="tablet:block body-6 hidden self-end text-gray-700">
         프로필 관리하기
       </Link>
     </div>
