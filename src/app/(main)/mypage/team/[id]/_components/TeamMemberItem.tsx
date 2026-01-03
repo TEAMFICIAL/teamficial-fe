d29ab86787a0f2c3dec291b3a2422d08929bb8d8
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-03 21:33:59 +0900

    #118 [FEAT] 참여중인 팀 팀원 페이지 반응형 추가

--- src/app/(main)/mypage/team/[id]/_components/TeamMemberItem.tsx
+++ src/app/(main)/mypage/team/[id]/_components/TeamMemberItem.tsx
@@ -9,14 +9,16 @@ interface TeamMemberItemProps {
 
 const TeamMemberItem = ({ member }: TeamMemberItemProps) => {
   return (
-    <div className="bg-gray-0 flex justify-between rounded-2xl border border-gray-300 px-14 py-8">
-      <div className="flex gap-7">
-        <MemberProfile member={member} />
-        <MemberInfo member={member} />
+    <>
+      <div className="tablet:hidden">{/* 기획 컨펌 후 추후 구현 */}</div>
+      <div className="tablet:flex bg-gray-0 hidden justify-between rounded-2xl border border-gray-300 px-14 py-8">
+        <div className="flex gap-7">
+          <MemberProfile member={member} />
+          <MemberInfo member={member} />
+        </div>
+        <MemberLinks links={member.links} />
       </div>
-
-      <MemberLinks links={member.links} />
-    </div>
+    </>
   );
 };
 
