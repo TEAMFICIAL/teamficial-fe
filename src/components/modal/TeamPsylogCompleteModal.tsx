8a9e75b28907b9ec27dac5a72d53aa99bb3b3f07
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-04 20:18:35 +0900

    #122 [FEAT] 팀피셜록 완료 모달 반응형 구현

--- src/components/modal/TeamPsylogCompleteModal.tsx
+++ src/components/modal/TeamPsylogCompleteModal.tsx
@@ -24,21 +24,28 @@ const TeamPsylogCompleteModal = ({
 
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col items-center gap-6.5">
-        <Image
-          src={`/icons/gray_teamficial_symbol.svg`}
-          alt="symbol"
-          width={55}
-          height={55}
-          className="flex flex-col"
-        />
-        <h3 className="title-3 text-gray-800">{userName}님의 팀피셜록 작성이 완료됐어요</h3>
-        <div className="flex gap-2">
-          <Button className="body-5 bg-gray-300 px-8 py-4 text-gray-800" onClick={handleHome}>
+      <div className="tablet:gap-6.5 flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center">
+          <Image
+            src={`/icons/gray_teamficial_symbol.svg`}
+            alt="symbol"
+            width={55}
+            height={55}
+            className="tablet:h-[55px] tablet:w-[55px] tablet:mb-6.5 h-[45px] w-[45px]"
+          />
+          <h3 className="tablet:title-3 body-7 text-gray-800">
+            {userName}님의 팀피셜록 작성이 완료됐어요
+          </h3>
+        </div>
+        <div className="tablet:gap-2 flex w-full gap-1">
+          <Button
+            className="tablet:body-5 body-7 tablet:px-8 tablet:py-4 bg-gray-300 px-4 py-2 whitespace-nowrap text-gray-800"
+            onClick={handleHome}
+          >
             홈으로
           </Button>
           <Button
-            className="text-gray-0 body-5 bg-primary-900 hover:bg-primary-700 px-40 py-4"
+            className="bg-primary-900 text-gray-0 tablet:body-5 body-7 hover:bg-primary-700 tablet:px-30 tablet:py-4 w-full px-4 py-2"
             onClick={handleTeampsylog}
           >
             팀피셜록 구경가기
