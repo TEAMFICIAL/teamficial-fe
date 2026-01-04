66bd162e9089543a3fe274e832c339af59cc2794
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-04 20:19:02 +0900

    #122 [FEAT] 질문 템플릿 타이틀 반응형 구현

--- src/app/(main)/question/[uuid]/_components/QuestionTitle.tsx
+++ src/app/(main)/question/[uuid]/_components/QuestionTitle.tsx
@@ -8,11 +8,13 @@ const QuestionTitle = ({ userName }: { userName: string }) => {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="title-3 mt-7 flex items-center gap-3">
+      <div className="tablet:title-3 body-5 tablet:mt-7 tablet:gap-3 mt-6 flex items-center gap-1">
         <PartDropdown selected={selectedPart} onSelect={setSelectedPart} />
         파트에서 본 {userName}님
       </div>
-      <p className="body-6 text-gray-700">각 질문별 원하는 질문을 하나씩 선택하여 응답해보세요</p>
+      <p className="tablet:body-6 body-10 text-gray-700">
+        각 질문별 원하는 질문을 하나씩 선택하여 응답해보세요
+      </p>
     </div>
   );
 };
