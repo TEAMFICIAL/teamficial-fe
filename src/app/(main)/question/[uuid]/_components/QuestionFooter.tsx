d165ecac6089fa372695cc7f1eb1524af120f8fe
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-04 20:37:41 +0900

    #122 [FIX] 코드래빗 수정사항 반영

--- src/app/(main)/question/[uuid]/_components/QuestionFooter.tsx
+++ src/app/(main)/question/[uuid]/_components/QuestionFooter.tsx
@@ -4,24 +4,14 @@ import Button from '@/components/common/button/Button';
 
 const QuestionFooter = () => {
   return (
-    <>
-      <div className="tablet:flex hidden w-full justify-end">
-        <Button
-          type="submit"
-          className="bg-primary-900 text-gray-0 body-3 hover:bg-primary-700 px-8 py-4"
-        >
-          팀피셜록 전달하기
-        </Button>
-      </div>
-      <div className="tablet:hidden bg-gray-0 fixed right-0 bottom-0 left-0 z-50 flex w-full gap-2 border-t border-gray-300 px-4 pt-3 pb-5">
-        <Button
-          type="submit"
-          className="body-5 bg-primary-900 text-gray-0 w-full px-5 py-4 text-center"
-        >
-          팀피셜록 전달하기
-        </Button>
-      </div>
-    </>
+    <div className="tablet:flex tablet:justify-end tablet:static tablet:w-auto tablet:border-t-0 tablet:px-0 tablet:pt-0 tablet:pb-0 tablet:bg-transparent bg-gray-0 fixed right-0 bottom-0 left-0 z-50 flex w-full border-t border-gray-300 px-4 pt-3 pb-5">
+      <Button
+        type="submit"
+        className="tablet:body-3 tablet:w-auto tablet:px-8 body-5 bg-primary-900 text-gray-0 hover:bg-primary-700 w-full px-5 py-4"
+      >
+        팀피셜록 전달하기
+      </Button>
+    </div>
   );
 };
 
