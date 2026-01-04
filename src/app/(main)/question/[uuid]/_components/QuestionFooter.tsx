c1cf30caa37ab23edd0e5308607de561b2179584
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-04 20:19:34 +0900

    #122 [FEAT] 질문템플릿 푸터 반응형 구현

--- src/app/(main)/question/[uuid]/_components/QuestionFooter.tsx
+++ src/app/(main)/question/[uuid]/_components/QuestionFooter.tsx
@@ -4,14 +4,24 @@ import Button from '@/components/common/button/Button';
 
 const QuestionFooter = () => {
   return (
-    <div className="flex w-full justify-end">
-      <Button
-        type="submit"
-        className="bg-primary-900 text-gray-0 body-3 hover:bg-primary-700 px-8 py-4"
-      >
-        팀피셜록 전달하기
-      </Button>
-    </div>
+    <>
+      <div className="tablet:flex hidden w-full justify-end">
+        <Button
+          type="submit"
+          className="bg-primary-900 text-gray-0 body-3 hover:bg-primary-700 px-8 py-4"
+        >
+          팀피셜록 전달하기
+        </Button>
+      </div>
+      <div className="tablet:hidden bg-gray-0 fixed right-0 bottom-0 left-0 z-50 flex w-full gap-2 border-t border-gray-300 px-4 pt-3 pb-5">
+        <Button
+          type="submit"
+          className="body-5 bg-primary-900 text-gray-0 w-full px-5 py-4 text-center"
+        >
+          팀피셜록 전달하기
+        </Button>
+      </div>
+    </>
   );
 };
 
