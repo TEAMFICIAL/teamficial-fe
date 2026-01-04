5b305820f833040f01b003eea2bd636bccf45fd5
김지원 <jiwonkim0531@gachon.ac.kr>
2026-01-04 21:26:39 +0900

    #125 [FEAT] 모바일용 푸터 구현

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,35 +2,86 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="mx-auto flex w-full justify-center bg-gray-600 px-10 pb-10">
-      <div className="flex w-[944px] items-center justify-between">
-        <div className="body-6 flex flex-col text-gray-100">
-          <Image src="/icons/footer-logo.svg" alt="logo" width={149} height={19} />
-          <p className="pb-1">Teamficial@gmail.com</p>
-          <p>© 2025.Teamficial. ALL rights reserved.</p>
-        </div>
-        <div className="flex flex-col justify-end gap-3">
-          <div className="flex justify-end gap-3">
-            <a href="mailto:Teamficial@gmail.com" aria-label="이메일 문의">
-              <Image src="/icons/mail.svg" alt="mail" width={24} height={24} />
-            </a>
+    <>
+      <footer className="tablet:hidden">
+        <div className="-mx-4 flex flex-col gap-4 bg-gray-100 py-6">
+          <div className="body-9 flex justify-between px-4">
+            <div className="flex flex-col gap-1">
+              <a
+                href="https://forms.gle/roAeMsKP9SjdaNQG9"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer"
+              >
+                문의하기
+              </a>
+              <p>개인정보처리방침</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p>광고상품 소개</p>
+              <p>서비스이용약관</p>
+            </div>
+          </div>
+          <div className="flex justify-between px-4">
+            <div>
+              <p className="body-11 text-gray-500">Teamficial@gmail.com</p>
+              <p className="body-11 text-gray-500">© 2025.Teamficial. ALL rights reserved.</p>
+            </div>
             <a
               href="https://www.instagram.com/teamficial.official/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="인스타그램"
             >
-              <Image src="/icons/instagram.svg" alt="instagram" width={24} height={24} />
+              <Image
+                src="/icons/mobile-instagram.svg"
+                alt="instagram"
+                width={24}
+                height={24}
+                className="justify-end"
+              />
             </a>
           </div>
-          <div className="body-7 flex gap-4 text-gray-100">
-            <p>서비스이용약관</p>
-            <p>개인정보처리방침</p>
-            <p>광고상품 소개</p>
+        </div>
+      </footer>
+      <footer className="tablet:flex mx-auto hidden w-full justify-center bg-gray-600 px-10 pb-10">
+        <div className="flex w-[944px] items-center justify-between">
+          <div className="body-6 flex flex-col text-gray-100">
+            <Image src="/icons/footer-logo.svg" alt="logo" width={149} height={19} />
+            <p className="pb-1">Teamficial@gmail.com</p>
+            <p>© 2025.Teamficial. ALL rights reserved.</p>
+          </div>
+          <div className="flex flex-col justify-end gap-3">
+            <div className="flex justify-end gap-3">
+              <a href="mailto:Teamficial@gmail.com" aria-label="이메일 문의">
+                <Image src="/icons/mail.svg" alt="mail" width={24} height={24} />
+              </a>
+              <a
+                href="https://www.instagram.com/teamficial.official/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="인스타그램"
+              >
+                <Image src="/icons/instagram.svg" alt="instagram" width={24} height={24} />
+              </a>
+            </div>
+            <div className="body-7 flex gap-4 text-gray-100">
+              <a
+                href="https://forms.gle/roAeMsKP9SjdaNQG9"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer"
+              >
+                문의하기
+              </a>
+              <p>서비스이용약관</p>
+              <p>개인정보처리방침</p>
+              <p>광고상품 소개</p>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
