8f07f624db11ddd12a7516db24f3ab4cd6dcd252
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-07 01:12:25 +0900

    #133 [CHORE] 모달 기본여백 수정 적용

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -26,7 +26,7 @@ const BaseModal = ({ isOpen, onClose, children, paddingClass }: BaseModalProps) 
           exit={{ opacity: 0 }}
         >
           <motion.div
-            className={`bg-gray-0 tablet:mx-0 tablet:w-auto tablet:max-w-none mx-5 w-full max-w-[400px] rounded-xl px-4 py-5 ${paddingClass ?? 'tablet:p-10'}`}
+            className={`bg-gray-0 desktop:mx-0 desktop:w-auto desktop:max-w-none mx-5 w-full max-w-[400px] rounded-xl px-4 py-5 ${paddingClass ?? 'desktop:p-7'}`}
             initial={{ scale: 0.9, opacity: 0 }}
             animate={{ scale: 1, opacity: 1 }}
             exit={{ scale: 0.9, opacity: 0 }}
