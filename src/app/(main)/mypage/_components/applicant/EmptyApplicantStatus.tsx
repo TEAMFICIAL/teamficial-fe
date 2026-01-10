8180245e9a57646d97a5d991ac8fac588e0dea61
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-01-10 18:54:47 +0900

    #135 [FIX] 마이페이지 비어있는 컴포넌트 css 수정

--- src/app/(main)/mypage/_components/applicant/EmptyApplicantStatus.tsx
+++ src/app/(main)/mypage/_components/applicant/EmptyApplicantStatus.tsx
@@ -12,7 +12,7 @@ const EmptyApplicantStatus = () => {
           전체보기
         </Link>
       </div>
-      <div className="tablet:body-3 body-7 tablet:mb-0 mb-7 flex h-[187px] w-full max-w-[944px] items-center justify-center rounded-2xl bg-gray-200 p-2.5 text-center text-gray-600">
+      <div className="body-3 tablet:flex mb-0 mb-7 hidden h-[187px] w-full max-w-[944px] items-center justify-center rounded-2xl bg-gray-200 p-2.5 text-center text-gray-600">
         <span>
           <Link href="/recruit" className="text-gray-700 underline">
             팀원 모집하기
@@ -20,6 +20,17 @@ const EmptyApplicantStatus = () => {
           페이지로 이동해서
           <br />
           함께할 프로젝트 팀원을 모집해보세요!
+        </span>
+      </div>
+      <div className="tablet:hidden body-8 mb-5 flex h-[164px] w-full max-w-[944px] items-center justify-center rounded-2xl bg-gray-200 p-2.5 text-center text-gray-600">
+        <span>
+          <Link href="/recruit" className="text-gray-700 underline">
+            팀원 모집하기
+          </Link>{' '}
+          페이지로 <br />
+          이동해서 함께할 프로젝트
+          <br />
+          팀원을 모집해보세요!
         </span>
       </div>
     </>
--- src/app/(main)/mypage/_components/applied/EmptyAppliedTeam.tsx
+++ src/app/(main)/mypage/_components/applied/EmptyAppliedTeam.tsx
@@ -9,7 +9,7 @@ const EmptyAppliedTeam = () => {
           전체보기
         </Link>
       </div>
-      <div className="tablet:body-3 body-9 tablet:h-[220px] tablet:mb-0 mb-7 flex h-[164px] w-full max-w-[944px] items-center justify-center rounded-2xl bg-gray-200 p-2.5 text-center text-gray-600">
+      <div className="body-3 tablet:flex mb-0 mb-7 hidden h-[164px] h-[220px] w-full max-w-[944px] items-center justify-center rounded-2xl bg-gray-200 p-2.5 text-center text-gray-600">
         <span>
           <Link href="/project" className="text-gray-700 underline">
             프로젝트
@@ -17,6 +17,17 @@ const EmptyAppliedTeam = () => {
           페이지로 이동해서
           <br />
           내가 가진 소프트 스킬로 팀에 지원해보세요!
+        </span>
+      </div>
+      <div className="tablet:hidden body-8 mb-7 flex h-[164px] w-full max-w-[944px] items-center justify-center rounded-2xl bg-gray-200 p-2.5 text-center text-gray-600">
+        <span>
+          <Link href="/project" className="text-gray-700 underline">
+            프로젝트
+          </Link>{' '}
+          페이지로 이동해서
+          <br />
+          내가 가진 소프트 스킬로 <br />
+          팀에 지원해보세요!
         </span>
       </div>
     </>
