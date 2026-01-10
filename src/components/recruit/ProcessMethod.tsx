a3847a3f4bf8f775516a0162da10e770a0b26a15
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-10 17:58:12 +0900

    #136 [CHORE] 라디오버튼 기본값 추가

--- src/components/recruit/ProcessMethod.tsx
+++ src/components/recruit/ProcessMethod.tsx
@@ -10,6 +10,8 @@ type Props = {
   control: Control<RecruitFormType>;
 };
 
+const ONLINE_VALUE = DURATION_OPTIONS[1]?.value;
+
 const ProcessMethod = ({ control }: Props) => {
   return (
     <div className="flex flex-col gap-4">
@@ -17,23 +19,28 @@ const ProcessMethod = ({ control }: Props) => {
       <Controller
         name="progressWay"
         control={control}
-        render={({ field: { value, onChange } }) => (
-          <div className="flex flex-col gap-2">
-            <div className="desktop:gap-6 desktop:flex-row flex flex-col gap-4">
-              {DURATION_OPTIONS.slice(1).map((option) => (
-                <RadioButton
-                  key={option.value}
-                  id={option.value}
-                  name="progressWay"
-                  value={option.value}
-                  checked={value === option.value}
-                  label={option.label}
-                  onChange={onChange}
-                />
-              ))}
+        render={({ field: { value, onChange } }) => {
+          if (value === undefined && ONLINE_VALUE) {
+            onChange(ONLINE_VALUE);
+          }
+          return (
+            <div className="flex flex-col gap-2">
+              <div className="desktop:gap-6 desktop:flex-row flex flex-col gap-4">
+                {DURATION_OPTIONS.slice(1).map((option) => (
+                  <RadioButton
+                    key={option.value}
+                    id={option.value}
+                    name="progressWay"
+                    value={option.value}
+                    checked={value === option.value}
+                    label={option.label}
+                    onChange={onChange}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          );
+        }}
       />
     </div>
   );
