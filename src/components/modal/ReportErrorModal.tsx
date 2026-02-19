8156db4ba73efaced4a1bb6e75e4f746454c8058
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-19 22:15:06 +0900

    #149 [CHORE] 신고모달 워딩 수정

--- src/components/modal/ReportErrorModal.tsx
+++ src/components/modal/ReportErrorModal.tsx
@@ -28,7 +28,6 @@ const ReportErrorModal = ({ isOpen, onClose }: BaseModalProps) => {
           className="desktop:mb-3 desktop:w-[55px] desktop:h-[55px] mb-2 flex h-11 w-11 flex-col"
         />
         <h3 className="body-7 desktop:title-3 text-gray-800">이미 신고된 키워드입니다.</h3>
-        <p className="body-10 desktop:body-6 text-gray-700">어쩌구저쩌구</p>
       </div>
     </BaseModal>
   );
