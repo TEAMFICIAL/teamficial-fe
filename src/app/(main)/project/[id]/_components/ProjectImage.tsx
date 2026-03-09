8b99361c8054b432f122860b0447193ae73b8530
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-03-09 16:25:36 +0900

    #160 [FEAT] 이미지 구역 추가

---
+++ src/app/(main)/project/[id]/_components/ProjectImage.tsx
@@ -0,0 +1,21 @@
+import { ResponseProject } from '@/types/project';
+import Image from 'next/image';
+
+const ProjectImage = ({ images }: ResponseProject) => {
+  return (
+    <div className="grid grid-cols-2 gap-5">
+      {images.map((img, index) => (
+        <Image
+          key={img.objectKey}
+          src={img.imageUrl}
+          alt={`Project Image ${index + 1}`}
+          className="h-auto rounded-lg"
+          width={500}
+          height={300}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectImage;
