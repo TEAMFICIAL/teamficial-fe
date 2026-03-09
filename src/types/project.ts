045102b1ea3b8b74f5d07c305cc0a238b480c007
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-03-09 16:09:50 +0900

    #160 [FEAT] 이미지 타입 추가

--- src/types/project.ts
+++ src/types/project.ts
@@ -16,6 +16,7 @@ export type Project = {
   content: string;
   title: string;
   recruitingPositions: RecruitingPosition[];
+  imageKeys?: string[];
 };
 
 export type CreateProject = Project & {
@@ -32,6 +33,7 @@ export type ResponseProject = Project & {
   alreadyApplied: boolean;
   writer: boolean;
   profileImageUrl: string;
+  imageUrls: string[];
 };
 
 export type DeleteProject = {
