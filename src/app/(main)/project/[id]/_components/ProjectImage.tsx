dace304e6e3d6a9638e5fb1cdd2d78a663806e8c
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-03-29 12:55:19 +0900

    #160 [CHORE] 이미지 배열 수정

--- src/app/(main)/project/[id]/_components/ProjectImage.tsx
+++ src/app/(main)/project/[id]/_components/ProjectImage.tsx
@@ -2,9 +2,10 @@ import { ResponseProject } from '@/types/project';
 import Image from 'next/image';
 
 const ProjectImage = ({ images }: ResponseProject) => {
+  if (!images || images.length === 0) return null;
   return (
-    <div className="grid grid-cols-2 gap-5">
-      {images?.map((img, index) => (
+    <div className="tablet:grid-cols-2 grid grid-cols-1 gap-5">
+      {images.map((img, index) => (
         <Image
           key={img.objectKey}
           src={img.imageUrl}
