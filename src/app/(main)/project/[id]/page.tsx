8ff1d36c0dd967f968b1951b1f8a8177a303f391
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-04-03 00:05:24 +0900

    #164 [FEAT] 프로젝트 페이지 메타데이터 추가

--- src/app/(main)/project/[id]/page.tsx
+++ src/app/(main)/project/[id]/page.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import ProjectInfo from './_components/ProjectInfo';
+import { Metadata } from 'next';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `TEAMFICIAL`,
+    description: `소프트스킬 팀빌딩 서비스, 팀피셜`,
+    openGraph: {
+      title: `TEAMFICIAL`,
+      description: `소프트스킬 팀빌딩 서비스, 팀피셜`,
+      images: [
+        {
+          url: 'https://www.teamficial.com/og/Teamficial_metatag_Image.jpg',
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+}
 
 type Props = {
   params: Promise<{ id: string }>;
